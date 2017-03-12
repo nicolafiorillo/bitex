c65add28d6aaeca30b19233df8ae2d5d4b54c1ac
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2017-03-13 00:11:06 +0100

    Serializing

--- lib/bitex/kernel.ex
+++ lib/bitex/kernel.ex
@@ -9,6 +9,9 @@ defmodule Bitex.Kernel do
           size = size - String.length(str)
           elem = (for <<b::8 <- str>>, do: b) ++ (for _ <- 1..size, do: 0)
           acc ++ elem
+        bin, acc when is_binary(bin) ->
+          elem = for <<b::8 <- bin>>, do: b
+          acc ++ elem
         elem, acc ->
           size = elem |> :binary.encode_unsigned() |> byte_size()
           str = <<elem :: little-size(size)-unit(8)>>
