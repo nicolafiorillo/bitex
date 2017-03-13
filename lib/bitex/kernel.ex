8c26304cdf75d3084a6e2cd0248b34506e93a607
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2017-03-13 23:18:39 +0100

    Var length decoding.

--- lib/bitex/kernel.ex
+++ lib/bitex/kernel.ex
@@ -2,7 +2,7 @@ defmodule Bitex.Kernel do
   def hash256(d) when is_binary(d) or is_bitstring(d), do: d |> sha256 |> sha256
   def hash160(d) when is_binary(d) or is_bitstring(d), do: d |> sha256 |> ripemd160
 
-  def serialize(a) when is_list(a) do
+  def encode(a) when is_list(a) do
     a |> Enum.reduce([],
       fn
         {size, str}, acc ->
@@ -18,6 +18,11 @@ defmodule Bitex.Kernel do
           acc ++ (for <<b::8 <- str>>, do: b)
       end)
   end
+
+  def decode([0xFF | int64]), do: Enum.slice(int64, 0, 8)
+  def decode([0xFE | int32]), do: Enum.slice(int32, 0, 4)
+  def decode([0xFD | int16]), do: Enum.slice(int16, 0, 2)
+  def decode([int8]), do: [int8]
 
   defp sha256(d) when is_binary(d) or is_bitstring(d), do: :crypto.hash(:sha256, d)
   defp ripemd160(d) when is_binary(d) or is_bitstring(d), do: :crypto.hash(:ripemd160, d)
