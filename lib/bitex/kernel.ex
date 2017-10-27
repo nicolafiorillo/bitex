264ec55fbc5f61bdf6673e05b86203a26bed0f90
Nicola Fiorillo <>
2017-10-27 07:37:38 +0200

    Fixing varint bug.

--- lib/bitex/kernel.ex
+++ lib/bitex/kernel.ex
@@ -4,7 +4,9 @@ defmodule Bitex.Kernel do
   """
 
   @doc """
-  Used to generate id for blocks and transactions (txid).
+  Used to generate id for blocks and transactions (txid):
+    block_id = hash256(block.header)
+    transaction_id = hash256(transaction)
   """
   def hash256(d) when is_binary(d) or is_bitstring(d), do: d |> sha256 |> sha256
 
@@ -13,12 +15,12 @@ defmodule Bitex.Kernel do
   """
   def hash160(d) when is_binary(d) or is_bitstring(d), do: d |> sha256 |> ripemd160
 
-  def encode(a) when is_list(a) do
+  def serialize(a) when is_list(a) do
     a |> Enum.reduce([],
       fn
         %Bitex.FixedLengthString{length: size, string: str}, acc ->
-          size = size - String.length(str)
-          elem = (for <<b::8 <- str>>, do: b) ++ (for _ <- 1..size, do: 0)
+          pad_size = size - String.length(str)
+          elem = (for <<b::8 <- str>>, do: b) ++ (for _ <- 1..pad_size, do: 0)
           acc ++ elem
         bin, acc when is_binary(bin) ->
           elem = for <<b::8 <- bin>>, do: b
@@ -30,15 +32,15 @@ defmodule Bitex.Kernel do
       end)
   end
 
-  def value_from_varint([0xFF | [b1 | [b2 | [b3 | [b4 | rest]]]]]), do: Enum.slice(rest, 0, merge_int(b1, b2, b3, b4))
-  def value_from_varint([0xFE | [b1 | [b2 | [b3 | rest]]]]), do: Enum.slice(rest, 0, merge_int(b1, b2, b3))
-  def value_from_varint([0xFD | [b1 | [b2 | rest]]]), do: Enum.slice(rest, 0, merge_int(b1, b2))
+  def value_from_varint([]), do: raise "invalid varint"
+  def value_from_varint([0xFF | payload]) when is_list(payload) and length(payload) >= 8, do: payload |> Enum.slice(0, 8)
+  def value_from_varint([0xFF | payload]) when is_list(payload), do: raise "invalid varint 64"
+  def value_from_varint([0xFE | payload]) when is_list(payload) and length(payload) >= 4, do: payload |> Enum.slice(0, 4)
+  def value_from_varint([0xFE | payload]) when is_list(payload), do: raise "invalid varint 32"
+  def value_from_varint([0xFD | payload]) when is_list(payload) and length(payload) >= 2, do: payload |> Enum.slice(0, 2)
+  def value_from_varint([0xFD | payload]) when is_list(payload), do: raise "invalid varint 16"
   def value_from_varint([first | _]), do: [first]
 
   defp sha256(d) when is_binary(d) or is_bitstring(d), do: :crypto.hash(:sha256, d)
   defp ripemd160(d) when is_binary(d) or is_bitstring(d), do: :crypto.hash(:ripemd160, d)
-
-  defp merge_int(b1, b2), do: b1 + b2 * 10
-  defp merge_int(b1, b2, b3), do: b1 + b2 * 10 + b3 * 100
-  defp merge_int(b1, b2, b3, b4), do: b1 + b2 * 10 + b3 * 100 + b4 * 1000
 end
--- test/bitex_test.exs
+++ test/bitex_test.exs
@@ -2,37 +2,37 @@ defmodule BitexTest do
   use ExUnit.Case
   doctest Bitex
 
-  test "serialize 1" do
+  test "serialize integers" do
     n8 = 0x01
     n16 = 0x4523
     n32 = 0xcdab8967
     n64 = 0xdebc9a78563412ef
 
     a = [n8, n16, n32, n64]
-    assert Bitex.Kernel.encode(a) == [0x01,
+    assert Bitex.Kernel.serialize(a) == [0x01,
                                           0x23, 0x45,
                                           0x67, 0x89, 0xAB, 0xCD,
                                           0xEF, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE]
   end
 
-  test "serialize 2" do
+  test "serialize fixed-length data: null-padded string" do
     n32 = 0x68f7a38b
     str = %Bitex.FixedLengthString{length: 10, string: "FooBar"}
     n16 = 0xee12
 
     a = [n32, str, n16]
-    assert Bitex.Kernel.encode(a) == [0x8B, 0xA3, 0xF7, 0x68,
+    assert Bitex.Kernel.serialize(a) == [0x8B, 0xA3, 0xF7, 0x68,
                                           0x46, 0x6F, 0x6F, 0x42, 0x61, 0x72, 0x00, 0x00, 0x00, 0x00,
                                           0x12, 0xEE]
   end
 
-  test "serialize 3" do
+  test "serialize fixed-length data: hashes" do
     message = "Hello Bitcoin!"
     prefix = 0xd17f
     suffix = 0x8c
 
     a = [prefix, Bitex.Kernel.hash256(message), suffix]
-    assert Bitex.Kernel.encode(a) == [0x7F, 0xD1,
+    assert Bitex.Kernel.serialize(a) == [0x7F, 0xD1,
                                           0x90, 0x98, 0x6E, 0xA4, 0xE2, 0x8B, 0x84, 0x7C,
                                           0xC7, 0xF9, 0xBE, 0xBA, 0x87, 0xEA, 0x81, 0xB2,
                                           0x21, 0xCA, 0x6E, 0xAF, 0x98, 0x28, 0xA8, 0xB0,
@@ -40,63 +40,63 @@ defmodule BitexTest do
                                           0x8C]
   end
 
-  test "value_from_varint int64 1" do
-    a = [0xFF, 0x04, 0x00, 0x00, 0x00, 0x05, 0x06, 0x07, 0x08]
-    assert Bitex.Kernel.value_from_varint(a) == [0x05, 0x06, 0x07, 0x08]
+  test "value_from_varint int64" do
+    a = [0xFF, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]
+    assert Bitex.Kernel.value_from_varint(a) == [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]
   end
 
-  test "value_from_varint int64 2" do
-    a = [0xFF, 0x08, 0x00, 0x00, 0x00, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C]
-    assert Bitex.Kernel.value_from_varint(a) == [0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C]
+  test "value_from_varint int64 and more" do
+    a = [0xFF, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09]
+    assert Bitex.Kernel.value_from_varint(a) == [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]
   end
 
-  test "value_from_varint int64 3" do
-    a = [0xFF, 0x08, 0x00, 0x00, 0x00, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F]
-    assert Bitex.Kernel.value_from_varint(a) == [0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C]
+  test "value_from_varint int64 but less" do
+    a = [0xFF, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
+    assert_raise RuntimeError, "invalid varint 64", fn -> Bitex.Kernel.value_from_varint(a) end
   end
 
-  test "value_from_varint int32 1" do
-    a = [0xFE, 0x01, 0x00, 0x00, 0x04]
-    assert Bitex.Kernel.value_from_varint(a) == [0x04]
+  test "value_from_varint int32" do
+    a = [0xFE, 0x01, 0x02, 0x03, 0x04]
+    assert Bitex.Kernel.value_from_varint(a) == [0x01, 0x02, 0x03, 0x04]
   end
 
-  test "value_from_varint int32 2" do
-    a = [0xFE, 0x02, 0x00, 0x00, 0x04, 0x04]
-    assert Bitex.Kernel.value_from_varint(a) == [0x04, 0x04]
+  test "value_from_varint int32 and more" do
+    a = [0xFE, 0x01, 0x02, 0x03, 0x04, 0x5]
+    assert Bitex.Kernel.value_from_varint(a) == [0x01, 0x02, 0x03, 0x04]
   end
 
-  test "value_from_varint int32 3" do
-    a = [0xFE, 0x02, 0x00, 0x00, 0x04, 0x04, 0x04, 0x04, 0x04]
-    assert Bitex.Kernel.value_from_varint(a) == [0x04, 0x04]
+  test "value_from_varint int32 but less" do
+    a = [0xFE, 0x01, 0x02, 0x03]
+    assert_raise RuntimeError, "invalid varint 32", fn -> Bitex.Kernel.value_from_varint(a) end
   end
 
-  test "value_from_varint int16 1" do
-    a = [0xFD, 0x01, 0x00, 0x05]
-    assert Bitex.Kernel.value_from_varint(a) == [0x05]
+  test "value_from_varint int16" do
+    a = [0xFD, 0x01, 0x02]
+    assert Bitex.Kernel.value_from_varint(a) == [0x01, 0x02]
   end
 
-  test "value_from_varint int16 2" do
-    a = [0xFD, 0x03, 0x00, 0x05, 0x05, 0x05]
-    assert Bitex.Kernel.value_from_varint(a) == [0x05, 0x05, 0x05]
+  test "value_from_varint int16 and more" do
+    a = [0xFD, 0x01, 0x02, 0x03]
+    assert Bitex.Kernel.value_from_varint(a) == [0x01, 0x02]
   end
 
-  test "value_from_varint int16 3" do
-    a = [0xFD, 0x02, 0x00, 0x05, 0x05, 0x05, 0x05, 0x05]
-    assert Bitex.Kernel.value_from_varint(a) == [0x05, 0x05]
+  test "value_from_varint int16 but less" do
+    a = [0xFD, 0x01]
+    assert_raise RuntimeError, "invalid varint 16", fn -> Bitex.Kernel.value_from_varint(a) end
   end
 
-  test "value_from_varint int8 1" do
+  test "value_from_varint int8" do
     a = [0x01]
     assert Bitex.Kernel.value_from_varint(a) == [0x01]
   end
 
-  test "value_from_varint int8 2" do
-    a = [0x01, 0x05]
+  test "value_from_varint int8 and more" do
+    a = [0x01, 0x02]
     assert Bitex.Kernel.value_from_varint(a) == [0x01]
   end
 
-  test "value_from_varint int8 3" do
-    a = [0x01, 0x05, 0x05]
-    assert Bitex.Kernel.value_from_varint(a) == [0x01]
+  test "value_from_varint empty" do
+    a = []
+    assert_raise RuntimeError, "invalid varint", fn -> Bitex.Kernel.value_from_varint(a) end
   end
 end
